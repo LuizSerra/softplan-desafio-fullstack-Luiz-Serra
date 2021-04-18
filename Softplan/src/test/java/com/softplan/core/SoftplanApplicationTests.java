9279ea41c7d727d83df60afd736783b4baff1cb7
Luiz Serra <lserra.trade@gmail.com>
2021-04-18 13:10:59 -0300

    Commit inicial

---
+++ Softplan/src/test/java/com/softplan/core/SoftplanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softplan.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftplanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
