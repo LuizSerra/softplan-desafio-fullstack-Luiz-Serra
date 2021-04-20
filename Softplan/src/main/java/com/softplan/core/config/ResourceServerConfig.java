0d7d637570c28a802dc0bcb6ff6c6a573b086ea0
Luiz Serra <lserra.trade@gmail.com>
2021-04-20 02:25:40 -0300

    Redefinindo permissoes

--- Softplan/src/main/java/com/softplan/core/config/ResourceServerConfig.java
+++ Softplan/src/main/java/com/softplan/core/config/ResourceServerConfig.java
@@ -24,7 +24,6 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-		.antMatchers("/usuarios/**").permitAll()
 		  .anyRequest().authenticated()
 		  .and()
 		  .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
